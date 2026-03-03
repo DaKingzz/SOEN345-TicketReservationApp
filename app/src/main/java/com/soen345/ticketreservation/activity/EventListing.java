a324326edbf02d9bc9f99954e08ee771878b4c30
robert-louis-lando <robertlouis_lando@outlook.com>
2026-03-03 12:32:11 -0500

    added-the-category-filters

--- app/src/main/java/com/soen345/ticketreservation/activity/EventListing.java
+++ app/src/main/java/com/soen345/ticketreservation/activity/EventListing.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.chip.ChipGroup;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.ListenerRegistration;
 import com.soen345.ticketreservation.R;
@@ -18,9 +19,11 @@ import com.soen345.ticketreservation.adapter.EventAdapter;
 import com.soen345.ticketreservation.auth.AuthManager;
 import com.soen345.ticketreservation.event.EventManager;
 import com.soen345.ticketreservation.model.Event;
+import com.soen345.ticketreservation.model.EventCategory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EventListing extends BaseActivity {
 
@@ -29,8 +32,10 @@ public class EventListing extends BaseActivity {
     private EventManager eventManager;
     private RecyclerView rvEvents;
     private EventAdapter eventAdapter;
-    private List<Event> eventList = new ArrayList<>();
+    private List<Event> allEvents = new ArrayList<>();
+    private List<Event> filteredEvents = new ArrayList<>();
     private ListenerRegistration eventListener;
+    private ChipGroup chipGroupFilters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +47,10 @@ public class EventListing extends BaseActivity {
 
         welcomeText = findViewById(R.id.welcome_text);
         rvEvents = findViewById(R.id.rvEvents);
+        chipGroupFilters = findViewById(R.id.chipGroupFilters);
 
         setupRecyclerView();
+        setupFilterChips();
         loadUserGreeting();
         initGoToCreateEventListener();
     }
@@ -63,9 +70,41 @@ public class EventListing extends BaseActivity {
     }
 
     private void setupRecyclerView() {
-        eventAdapter = new EventAdapter(eventList);
+        eventAdapter = new EventAdapter(filteredEvents);
         rvEvents.setLayoutManager(new LinearLayoutManager(this));
         rvEvents.setAdapter(eventAdapter);
+    }
+
+    private void setupFilterChips() {
+        chipGroupFilters.setOnCheckedChangeListener((group, checkedId) -> {
+            filterEvents();
+        });
+    }
+
+    private void filterEvents() {
+        int checkedId = chipGroupFilters.getCheckedChipId();
+        String selectedCategory = "";
+
+        if (checkedId == R.id.chipConcert) {
+            selectedCategory = EventCategory.CONCERT.toString();
+        } else if (checkedId == R.id.chipMovie) {
+            selectedCategory = EventCategory.MOVIE.toString();
+        } else if (checkedId == R.id.chipSport) {
+            selectedCategory = EventCategory.SPORT.toString();
+        } else if (checkedId == R.id.chipTravel) {
+            selectedCategory = EventCategory.TRAVEL.toString();
+        }
+
+        filteredEvents.clear();
+        if (selectedCategory.isEmpty()) {
+            filteredEvents.addAll(allEvents);
+        } else {
+            String finalSelectedCategory = selectedCategory;
+            filteredEvents.addAll(allEvents.stream()
+                    .filter(e -> e.getCategory() != null && e.getCategory().equalsIgnoreCase(finalSelectedCategory))
+                    .collect(Collectors.toList()));
+        }
+        eventAdapter.notifyDataSetChanged();
     }
 
     private void startListeningForEvents() {
@@ -73,9 +112,9 @@ public class EventListing extends BaseActivity {
             eventListener.remove();
         }
         eventListener = eventManager.listenToEvents(events -> {
-            eventList.clear();
-            eventList.addAll(events);
-            eventAdapter.notifyDataSetChanged();
+            allEvents.clear();
+            allEvents.addAll(events);
+            filterEvents();
         });
     }
 
